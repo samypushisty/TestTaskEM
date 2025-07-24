009ab441cb8832c1f19106bd103e671607ef2c33
samypushisty <pomidorik228@gmail.com>
2025-07-24 15:26:05 +0300

    Base

---
+++ application/core/models/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
+from core.config import settings
+
+
+class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
---
+++ application/main.py
@@ -0,0 +1,47 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+
+from core.config import settings
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(
+
+    lifespan=lifespan,
+    title="App",
+)
+
+origins = [
+    "*"
+    # "http://localhost:5173",
+    # "http://127.0.0.1:5173",
+]
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@main_app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        reload=True,
+        host=settings.run.host,
+        port=settings.run.port
+    )
